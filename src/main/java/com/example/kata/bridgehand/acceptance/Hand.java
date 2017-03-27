a19860b43c3e42d4aae371b4271c2ed465f67a49
Alvaro Garcia <derecerca@gmail.com>
2017-03-27 10:53:14 +0100

    Refactor: replace function with method call

--- src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
+++ src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
@@ -1,6 +1,5 @@
 package com.example.kata.bridgehand.acceptance;
 
-import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
 public class Hand {
@@ -8,7 +7,7 @@ public class Hand {
 
     public Hand (final String... suitDescriptions) {
         total = allCardsInHand(suitDescriptions)
-                .map(valueOfCard())
+                .map(this::valueOfCard)
                 .sum();
     }
 
@@ -16,19 +15,16 @@ public class Hand {
         return String.join("", suitDescriptions).chars();
     }
 
-    private IntUnaryOperator valueOfCard () {
-        return (char_) -> {
-            int result = 0;
-            switch (char_) {
-                case (int) 'A':
-                    result = 4;
-                    break;
-                case (int) 'K':
-                    result = 3;
-                    break;
-            }
-            return result;
-        };
+    private Integer valueOfCard (int card) {
+        int result = 0;
+        switch (card) {
+            case (int) 'A':
+                result = 4;
+                break;
+            case (int) 'K':
+                result = 3;
+                break;
+        }
+        return result;
     }
-
 }
