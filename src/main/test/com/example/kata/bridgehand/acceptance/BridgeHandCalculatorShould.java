0adb4b4051bb738f85e412fe06f42566e4b4e876
Alvaro Garcia <derecerca@gmail.com>
2017-03-27 11:02:06 +0100

    Red: points for spades

--- src/main/test/com/example/kata/bridgehand/acceptance/BridgeHandCalculatorShould.java
+++ src/main/test/com/example/kata/bridgehand/acceptance/BridgeHandCalculatorShould.java
@@ -8,6 +8,11 @@ import static org.hamcrest.core.Is.is;
 
 public class BridgeHandCalculatorShould {
 
+    private static final Hand MAXIMUM_POINTS_HAND = new Hand("SAKQ",
+            "HAKQ",
+            "DAKQ",
+            "CAKQJ");
+
     @Test
     public void tell_no_points_on_a_hand_made_of_pointless_cards () {
 
@@ -16,5 +21,10 @@ public class BridgeHandCalculatorShould {
                 "D???",
                 "C???");
         assertThat(hand.total, is(0));
+    }
+
+    @Test
+    public void points_for_spades_suit () {
+        assertThat(MAXIMUM_POINTS_HAND.spades, is(9));
     }
 }
