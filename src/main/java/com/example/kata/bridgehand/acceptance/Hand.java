b2dab77d6c819df18ddc898526f28feb2576822d
Alvaro Garcia <derecerca@gmail.com>
2017-03-27 10:45:46 +0100

    Refactor: reorder the operations
    instead of stream<stream<char>> -> stream<char>

--- src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
+++ src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
@@ -1,28 +1,24 @@
 package com.example.kata.bridgehand.acceptance;
 
-import java.util.stream.Stream;
-
 public class Hand {
     public final int total;
 
     public Hand (final String... descriptions) {
-        total = Stream.of(descriptions)
-                .map(description -> description
-                        .chars()
+        total = String.join("", descriptions).chars()
                         .map((char_) -> {
                             int result = 0;
-                            switch (char_){
+                            switch (char_) {
                                 case (int) 'A':
                                     result = 4;
                                     break;
                                 case (int) 'K':
                                     result = 3;
-                                break;
+                                    break;
                             }
                             return result;
                         })
-                .reduce(0, (a, b) -> a + b))
-                .reduce(0, (a, b) -> a + b).intValue();
+                        .reduce(0, (a, b) -> a + b);
+
     }
 
 }
