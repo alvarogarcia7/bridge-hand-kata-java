043aaf327068c4493d1ea2a0dbfd69ed81f4443f
Alvaro Garcia <derecerca@gmail.com>
2017-03-27 10:48:16 +0100

    Refactor: extract explaining function

--- src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
+++ src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
@@ -1,24 +1,30 @@
 package com.example.kata.bridgehand.acceptance;
 
+import java.util.function.IntUnaryOperator;
+
 public class Hand {
     public final int total;
 
     public Hand (final String... descriptions) {
         total = String.join("", descriptions).chars()
-                        .map((char_) -> {
-                            int result = 0;
-                            switch (char_) {
-                                case (int) 'A':
-                                    result = 4;
-                                    break;
-                                case (int) 'K':
-                                    result = 3;
-                                    break;
-                            }
-                            return result;
-                        })
+                        .map(valueOfCard())
                         .reduce(0, (a, b) -> a + b);
 
+    }
+
+    private IntUnaryOperator valueOfCard () {
+        return (char_) -> {
+            int result = 0;
+            switch (char_) {
+                case (int) 'A':
+                    result = 4;
+                    break;
+                case (int) 'K':
+                    result = 3;
+                    break;
+            }
+            return result;
+        };
     }
 
 }
