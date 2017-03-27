79af55a53af6d34588bb3dd3a5292f9e10a2bfac
Alvaro Garcia <derecerca@gmail.com>
2017-03-27 10:22:38 +0100

    green

--- src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
+++ src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
@@ -4,6 +4,7 @@ public class Hand {
     public final int total;
 
     public Hand (final String... descriptions) {
-        total = (int) (descriptions[0].chars().filter(char_ -> ((int)'A') == (char_)).count() * 4);
+        total = (int) (descriptions[0].chars().filter(char_ -> ((int)'A') == (char_)).count() * 4) + (int)
+                (descriptions[1].chars().filter(char_ -> ((int)'A') == (char_)).count() * 4);
     }
 }
