b9ca4297861bfa7ca03a9a6d3fe3e3f015756de5
Alvaro Garcia <derecerca@gmail.com>
2017-03-27 10:31:15 +0100

    Refactor: replace algorithm
    from manual iteration to stream

--- src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
+++ src/main/java/com/example/kata/bridgehand/acceptance/Hand.java
@@ -1,12 +1,16 @@
 package com.example.kata.bridgehand.acceptance;
 
+import java.util.stream.Stream;
+
 public class Hand {
     public final int total;
 
     public Hand (final String... descriptions) {
-        total = (int) (descriptions[0].chars().filter(char_ -> ((int)'A') == (char_)).count() * 4) + (int)
-                (descriptions[1].chars().filter(char_ -> ((int)'A') == (char_)).count() * 4)+(int)(descriptions[2]
-                .chars().filter(char_ -> ((int)'A') == (char_)).count() * 4)+(int)(descriptions[3]
-                .chars().filter(char_ -> ((int)'A') == (char_)).count() * 4);
+        total = Stream.of(descriptions)
+                .map(description -> description
+                        .chars()
+                        .filter(char_ -> ((int) 'A') == (char_))
+                        .count() * 4)
+                .reduce(0L, (a, b) -> a + b).intValue();
     }
 }
